f7bd968509c7207fb0cdd62e1947558fefd3ee7f
PierreMV04 <kingalex200429@gmail.com>
2025-11-16 19:41:30 -0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,31 @@
+import requests
+from telegram import Bot
+from datetime import datetime
+import os
+
+API_KEY_WEATHER = os.getenv("API_KEY_WEATHER")
+API_TELEGRAM = os.getenv("API_TELEGRAM")
+CHANNEL = os.getenv("CHANNEL")
+
+LAT = -0.75
+LON = -78.95
+
+def obtener_clima():
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LON}&appid={API_KEY_WEATHER}&units=metric&lang=es"
+    data = requests.get(url).json()
+
+    temp = data["main"]["temp"]
+    clima = data["weather"][0]["description"]
+
+    return f"""
+🌤 *Clima Aproximado Chugchilán (basado en Sigchos)*
+🕒 {datetime.now().strftime('%d-%m-%Y %H:%M')}
+🌡 Temperatura: {temp}°C
+☁ Condición: {clima}
+"""
+
+def enviar():
+    bot = Bot(API_TELEGRAM)
+    bot.send_message(chat_id=CHANNEL, text=obtener_clima(), parse_mode="Markdown")
+
+enviar()
